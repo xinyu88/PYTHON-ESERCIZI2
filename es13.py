c16b8a44246b454bc463cde78263838bc40a6ca0
Xinyu <71143643+xinyu88@users.noreply.github.com>
2020-12-30 13:33:36 +0100

    Update es13.py

--- es13.py
+++ es13.py
@@ -1,6 +1,6 @@
 #Verifica se un numero è pari o dispari (un numero è pari quando il resto della divisione intera per 2 è uguale a 0)
 
-numero = int(input("inserisci un numero a piacere, per verificare se è pari o dispari"))
+numero = int(input("inserisci un numero a piacere, per verificare se è pari o dispari = "))
 
 if numero % 2 == 0 :
     print("il numero inserito", numero, "è pari")
