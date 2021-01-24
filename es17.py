c323d2538ab6f554d9209f0b6248ee972ae286e1
Xinyu <71143643+xinyu88@users.noreply.github.com>
2021-01-24 14:37:32 +0100

    Update es17.py

--- es17.py
+++ es17.py
@@ -6,12 +6,12 @@ capitali = ["Roma", "Londra", "Parigi", "Pechino", "Mosca", "Berlino", "Tokyo" ]
 d = { } 
 
 for c in range(len(capitali)) : 
-    d[capitali[c]] = nazioni[c]  #assegno alla 
+    d[capitali[c]] = nazioni[c] 
 
 print("Di quale capitale vuoi saperne la nazione ? ", capitali)
 risposta = input("capitale = ").capitalize()
 
-if risposta in capitali : #
+if risposta in capitali : 
     print("la nazione è", d[risposta])
 else : 
     print("la capitale non appare nell'elenco")
