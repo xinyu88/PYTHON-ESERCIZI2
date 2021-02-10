76630dcd8853443dbecc06fd3a57ef516ea6f582
Xinyu <71143643+xinyu88@users.noreply.github.com>
2021-02-10 22:55:43 +0100

    Update es25.py

--- es25.py
+++ es25.py
@@ -7,7 +7,7 @@ sono stati assegnati, riducendo a uno i voti uguali.
 s = 0 # conto i studenti
 #chiedo i voti ai studenti 
 lista = {} 
-for e in range(4) : 
+for e in range(30) : 
     print("studente", s)
     voto = int(input("quanto hai preso?"))
     lista[s] = voto #studente come chiave e voto come valore
@@ -22,4 +22,4 @@ for e in ordine_crescente :
     c = ordine_crescente.count(e)
     if c >= 2 :
         ordine_crescente.remove(e) #rimuovo a uno i voti uguali
-        print("ecco la nuova lista", ordine_crescente) #in caso i voti siano uguali, ridumuove a uno.
+        print("ecco la nuova lista", ordine_crescente) #in caso i voti siano uguali, rimuovere a uno.
