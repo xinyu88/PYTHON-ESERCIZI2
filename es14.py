fcbe59f3c8a6c5056819eb593743d706e6da0a23
Xinyu <71143643+xinyu88@users.noreply.github.com>
2021-02-15 13:26:24 +0100

    aggiungo es14

--- es14.py
+++ es14.py
@@ -1,15 +1,14 @@
-#Crea un programma che scriva la differenza di due numeri a e b se il loro prodotto è maggiore di 10,
-#oppure la loro somma se il prodotto è minore o uguale a 10.
-#Esegui poi il programma con diverse coppie di valori per a e b: (-5, 2),(5, -5), (10, 2), (-4, -5), (5, 4), (-3, -2).
-
-a = int(input("inserisci un numero a : "))
-b = int(input("inserisci un numero b : "))
-prodotto = a * b 
-
-if prodotto > 10 : 
-    print("il prodotto di", a, "e", b, "ovvero", a * b, "è maggiore di 10, allora la differenza di", a, "-", b, "è", a-b)
-elif prodotto == 10 : 
-    print("il prodotto di", a, "e", b, "ovvero",  a * b, "è uguale a 10, allora la somma di", a, " + ", b, "è", a+b)
-else : 
-    print("il prodotto di", a, "e", b, "ovvero", a * b, "è minore di 10, allora la somma di", a, " + ", b, "è", a+b)
-    
+'''
+ES 14 pag 189.
+Organizza in una struttura dati i valori degli occupati negli ultimi dieci anni. Utilizza un dizionario, assegnando 
+il ruolo di chiave all'anno. Inserisci i dati da tastiera e memorizzari nel continentore. Calcola poi il valore medio dei dieci anni e 
+visualizzane il risultato 
+'''
+d = {}
+for v in range(1, 3) :
+    print("Anno", v) 
+    dato = int(input("Numero degli occupati :"))
+    d["anno",v] = dato
+print(d)
+media = sum(d.values())/len(d)
+print("Ecco la media", float(media))
