61a9ea5bef66890072a7315c76c8ea4e9102e11e
Xinyu <71143643+xinyu88@users.noreply.github.com>
2021-02-21 19:53:12 +0100

    Update classe4.py

--- classe4.py
+++ classe4.py
@@ -1,35 +1,8 @@
 '''
 Aggiungi alla classe Atleta un metodo per visualizzare i dati del giocatore
-
-class Atleta(): #definisco la classe 
-    def __init__(self, name, age, weight, visitaMedica): #inizializzazione attributi  self identifica la calsse stessa e consente al metodo di utilizzare gli attributi
-        self.name = name
-        self.age = age
-        self.weight = weight
-        self.visitaMedica = visitaMedica
-    
-    def info(self): 
-        print(self.name, self.age, self.weight)
-        if self.visitaMedica == True :
-            print(self.name, "ha ricevuto la visita medica")
-        if self.visitaMedica == False: 
-            print(self.name,"non ha ricevuto la visita medica")
-    def team(self, name_team): 
-        self.name_team = name_team
-        print(self.name, self.age, self.weight, self.name_team)
-    def dati(self) : 
-        d = {"nome" : self.name, "età" : self.age, "peso" : self.weight}
-        print(d)
-
-a1 = Atleta("Alice", 19, "49 kg", True)
-a1.team("Inter")
-a1.dati()
-a2 = Atleta("Michele", 34, "78 kg", False)
-a2.team("Reggiana")
-a2.dati()
 '''
-class Atleta(): #definisco la classe 
-    def __init__(self, name, age, weight, visitaMedica): #inizializzazione attributi  self identifica la calsse stessa e consente al metodo di utilizzare gli attributi
+class Atleta(): 
+    def __init__(self, name, age, weight, visitaMedica):
         self.name = name
         self.age = age
         self.weight = weight
